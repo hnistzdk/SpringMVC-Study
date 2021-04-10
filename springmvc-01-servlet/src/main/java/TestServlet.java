c5776d3fe7d649c3425a077365b297cbea5eeac7
hnist <369365576@qq.com>
2021-04-10 20:14:08 +0800

    4.10

---
+++ springmvc-01-servlet/src/main/java/TestServlet.java
@@ -0,0 +1,26 @@
+/**
+ * @author zdk
+ * @date 2021/4/10 20:06
+ */
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username= request.getParameter("username");
+        if(username.equals("zdk")){
+            request.getSession().setAttribute("msg", "张迪凯");
+        }else {
+            request.getSession().setAttribute("msg", "不是张迪凯");
+        }
+        request.getRequestDispatcher("/WEB-INF/success.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+}
---
+++ springmvc-01-servlet/web/form.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zdk
+  Date: 2021/4/10
+  Time: 20:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<form action="/hello" method="get">
+    <input name="username" type="text">
+</form>
+</body>
+</html>
